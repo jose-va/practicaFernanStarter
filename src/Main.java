905a51001c6ba98c57be962696723a2948482cc6
Jose Vilchez <jose.vilchez.1106@fernando3martos.com>
2024-11-20 10:56:59 +0100

    primer commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner s = new Scanner(System.in);
+
+       String administrador= "Jose";
+       String gestor= "Pablo";
+       String inversor1= "Eladio";
+       String inversor2= "Toranzo";
+
+        System.out.println("Bienvenido");
+        System.out.println("Por favor, introduzca su nombre de usuario");
+        String usuario= s.nextLine();
+
+        while (!usuario.equals(administrador.toLowerCase()) || !usuario.equals(gestor.toLowerCase())
+                || !usuario.equals(inversor1.toLowerCase()) || !usuario.equals(inversor2.toLowerCase())){
+            System.out.println("Por favor, introduzca su nombre de usuario");
+            usuario= s.nextLine();
+        }
+
     }
 }
