06038be8657d8cfb0ebea92606121e78e10b1c80
Povaldev <pablovalderasdoblas.dev@gmail.com>
2024-11-29 09:22:48 +0100

    version 6.0

--- src/ApartadoInversiones.java
+++ src/ApartadoInversiones.java
@@ -15,9 +15,9 @@ public class ApartadoInversiones {
         String nombreProyectoC = "";
         //Aportación del inversor 1 a diferentes proyectos
         int aportacionI1 =0;
-        boolean inversionA1 = false;
-        boolean inversionB1 = false;
-        boolean inversionC1 = false;
+        boolean inversionA = false;
+        boolean inversionB = false;
+        boolean inversionC = false;
         //Aportación del inversor 2 a diferentes proyectos
         int aportacionI2=0;
         boolean inversionA2 = false;
@@ -33,9 +33,17 @@ public class ApartadoInversiones {
             System.out.println("Actualmente, no ha realizado ninguna inversión");
             System.out.println("¿Desea invertir en algún proyecto? (s/n)");
             String respuesta2 = s.nextLine();
+            while (!respuesta2.equals("s")){
+                if (respuesta2.equals("n")){
+                    return;
+                }
+                System.out.println("Introduce una opcion valida (s/n)");
+                System.out.println("¿Desea invertir en algún proyecto? (s/n)");
+                respuesta2 = s.nextLine();
+            }
             if (respuesta2.equals("s")) {
-                int opcion2 = 0;
-                while (opcion2 != 1 || opcion2 != 2 || opcion2 != 3) {
+                int opcion3 = 0;
+                while (opcion3 != 1 || opcion3 != 2 || opcion3 != 3) {
                     System.out.println("¿En que proyecto desea invertir? Introduce el número correspondiente");
                     System.out.println("Proyecto 1: " + nombreProyectoA);
                     System.out.println("Proyecto 2: " + nombreProyectoB);
@@ -46,29 +54,26 @@ public class ApartadoInversiones {
                             System.out.println("La versión necesaria de este proyecto es de " + cantidadA);
                             System.out.println("¿Que cantidad desea aportar?");
                             cantidadAportadaA = s.nextInt();
-                            inversionA1 = true;
+                            inversionA = true;
                             break;
                         case 2:
                             System.out.println("La versión necesaria de este proyecto es de " + cantidadB);
                             System.out.println("¿Que cantidad desea aportar?");
                             cantidadAportadaB = s.nextInt();
-                            inversionB1 = true;
+                            inversionB = true;
                             break;
                         case 3:
                             System.out.println("La versión necesaria de este proyecto es de " + cantidadC);
                             System.out.println("¿Que cantidad desea aportar?");
                             cantidadAportadaC = s.nextInt();
-                            inversionC1 = true;
+                            inversionC = true;
                             break;
                         default:
                             System.out.println("Debe de introducir la opción correspondiente correcta");
                     }
                 }
-            } else if (!respuesta2.equals("n")) {
-                System.out.println("Introduce una opcion valida (s/n)");
             }
         } else if (inversiones>0){
-
 
         }
     }
--- src/CopiaSeguridadMain.java
+++ src/CopiaSeguridadMain.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-
-public class Main {
+// Último acceso: 29/11/2024
+public class CopiaSeguridadMain {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
@@ -122,92 +122,57 @@ public class Main {
 
                     switch (menu) {
                         case 1:
-                            System.out.println("-----MIS INVERSIONES-----");
-                            System.out.println("1. Crear inversiones");
-                            System.out.println("2. Consultar inversiones");
-                            System.out.println("3. Salir");
-                            opcion= Integer.parseInt(s.next());
-                            switch (opcion){
-                                case 1:
-                                    System.out.println("¿En qué proyecto desea invertir?");
-                                    System.out.println("Proyecto 1: " + nombreProyectoA);
-                                    System.out.println("Proyecto 2: " + nombreProyectoB);
-                                    System.out.println("Proyecto 3: " + nombreProyectoC);
-                                    aportacionI1 = Integer.parseInt(s.nextLine());
-                                    switch (aportacionI1) {
-                                        case 1:
-                                            System.out.println("La versión necesaria de este proyecto es de " + cantidadA);
-                                            System.out.println("¿Que cantidad desea aportar?");
-                                            cantidadAportadaA = s.nextInt();
-                                            inversionA = true;
-                                            break;
-                                        case 2:
-                                            System.out.println("La versión necesaria de este proyecto es de " + cantidadB);
-                                            System.out.println("¿Que cantidad desea aportar?");
-                                            cantidadAportadaB = s.nextInt();
-                                            inversionB = true;
-                                            break;
-                                        case 3:
-                                            System.out.println("La versión necesaria de este proyecto es de " + cantidadC);
-                                            System.out.println("¿Que cantidad desea aportar?");
-                                            cantidadAportadaC = s.nextInt();
-                                            inversionC = true;
-                                            break;
-                                        default:
-                                            System.out.println("Debe de introducir la opción correspondiente correcta");
-                                            break;
+                            System.out.println("MIS INVERSIONES");
+
+                            if (inversiones == 0) {
+                                System.out.println("Actualmente, no ha realizado ninguna inversión");
+                                System.out.println("¿Desea invertir en algún proyecto? (s/n)");
+                                String respuesta2 = s.nextLine();
+                                while (!respuesta2.equals("s")){
+                                    if (respuesta2.equals("n")){
+                                        return;
                                     }
-                                case 2:
-                                    if (inversiones == 0) {
-                                        System.out.println("Actualmente, no ha realizado ninguna inversión");
-                                        System.out.println("¿Desea invertir en algún proyecto? (s/n)");
-                                        String respuesta2 = s.nextLine();
-                                        while (!respuesta2.equals("s")){
-                                            if (respuesta2.equals("n")){
-                                                return;
-                                            }
-                                            System.out.println("Introduce una opcion valida (s/n)");
-                                            System.out.println("¿Desea invertir en algún proyecto? (s/n)");
-                                            respuesta2 = s.nextLine();
+                                    System.out.println("Introduce una opcion valida (s/n)");
+                                    System.out.println("¿Desea invertir en algún proyecto? (s/n)");
+                                    respuesta2 = s.nextLine();
+                                }
+                                if (respuesta2.equals("s")) {
+                                    int opcion3 = 0;
+                                    while (opcion3 != 1 || opcion3 != 2 || opcion3 != 3) {
+                                        System.out.println("¿En que proyecto desea invertir? Introduce el número correspondiente");
+                                        System.out.println("Proyecto 1: " + nombreProyectoA);
+                                        System.out.println("Proyecto 2: " + nombreProyectoB);
+                                        System.out.println("Proyecto 3: " + nombreProyectoC);
+                                        aportacionI1 = Integer.parseInt(s.nextLine());
+                                        switch (aportacionI1) {
+                                            case 1:
+                                                System.out.println("La versión necesaria de este proyecto es de " + cantidadA);
+                                                System.out.println("¿Que cantidad desea aportar?");
+                                                cantidadAportadaA = s.nextInt();
+                                                inversionA = true;
+                                                break;
+                                            case 2:
+                                                System.out.println("La versión necesaria de este proyecto es de " + cantidadB);
+                                                System.out.println("¿Que cantidad desea aportar?");
+                                                cantidadAportadaB = s.nextInt();
+                                                inversionB = true;
+                                                break;
+                                            case 3:
+                                                System.out.println("La versión necesaria de este proyecto es de " + cantidadC);
+                                                System.out.println("¿Que cantidad desea aportar?");
+                                                cantidadAportadaC = s.nextInt();
+                                                inversionC = true;
+                                                break;
+                                            default:
+                                                System.out.println("Debe de introducir la opción correspondiente correcta");
+                                                break;
                                         }
-                                        if (respuesta2.equals("s")) {
-                                            int opcion3 = 0;
-                                            while (opcion3 != 1 || opcion3 != 2 || opcion3 != 3) {
-                                                System.out.println("¿En que proyecto desea invertir? Introduce el número correspondiente");
-                                                System.out.println("Proyecto 1: " + nombreProyectoA);
-                                                System.out.println("Proyecto 2: " + nombreProyectoB);
-                                                System.out.println("Proyecto 3: " + nombreProyectoC);
-                                                aportacionI1 = Integer.parseInt(s.nextLine());
-                                                switch (aportacionI1) {
-                                                    case 1:
-                                                        System.out.println("La versión necesaria de este proyecto es de " + cantidadA);
-                                                        System.out.println("¿Que cantidad desea aportar?");
-                                                        cantidadAportadaA = s.nextInt();
-                                                        inversionA = true;
-                                                        break;
-                                                    case 2:
-                                                        System.out.println("La versión necesaria de este proyecto es de " + cantidadB);
-                                                        System.out.println("¿Que cantidad desea aportar?");
-                                                        cantidadAportadaB = s.nextInt();
-                                                        inversionB = true;
-                                                        break;
-                                                    case 3:
-                                                        System.out.println("La versión necesaria de este proyecto es de " + cantidadC);
-                                                        System.out.println("¿Que cantidad desea aportar?");
-                                                        cantidadAportadaC = s.nextInt();
-                                                        inversionC = true;
-                                                        break;
-                                                    default:
-                                                        System.out.println("Debe de introducir la opción correspondiente correcta");
-                                                        break;
-                                                }
-                                            }
-                                        }
-                                    } else if (inversiones>0){
-                                        System.out.println("Consulta de inversiones");
                                     }
-                                    break;
+                                }
+                            } else if (inversiones>0){
+                                System.out.println("Consulta de inversiones");
                             }
+                            break;
                         case 2:
                             System.out.println("CONSULTA DE PROYECTOS");
                             System.out.println();
@@ -237,7 +202,7 @@ public class Main {
                                 System.out.println("1. Saldo actual");
                                 System.out.println("2. Añadir saldo");
                                 System.out.println("3. Salir");
-                                opcion= Integer.parseInt(s.next());
+                                opcion=s.nextInt();
                                 switch (opcion){
                                     case 1:
                                         if(saldoTotal==0){
